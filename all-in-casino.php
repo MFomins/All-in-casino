8d97280a7d77cbd429a2e14c14b5b3318c66183d
Kristaps Ritins <ritins.kristaps@gmail.com>
2020-08-07 11:49:05 +0300

    style changes

--- all-in-casino.php
+++ all-in-casino.php
@@ -11,7 +11,7 @@
  * Plugin Name:       All In Casino
  * Plugin URI:        yoursite.lv
  * Description:       All In Casino plugin
- * Version:           1.0.16
+ * Version:           1.0.17
  * Author:            KR
  * Author URI:        yoursite.lv
  * License:           GPL-2.0+
@@ -30,7 +30,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'ALL_IN_CASINO_VERSION', '1.0.16' );
+define( 'ALL_IN_CASINO_VERSION', '1.0.17' );
 
 define('ALL_IN_CASINO_NAME', 'all-in-casino');
 
