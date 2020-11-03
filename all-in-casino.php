fb45ee40601659feb14bf66742688df58d8a5c4f
Kristaps Ritins <kristaps@yoursite.lv>
2020-11-03 21:10:29 +0200

    styles

--- all-in-casino.php
+++ all-in-casino.php
@@ -30,7 +30,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'ALL_IN_CASINO_VERSION', '1.0.42' );
+define( 'ALL_IN_CASINO_VERSION', '1.0.43' );
 
 define('ALL_IN_CASINO_NAME', 'all-in-casino');
 
